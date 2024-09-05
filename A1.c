9face7e4c5c86bad3b25e62dc33bf1846d7927e4
Suhani Mathur <suhani.mathur@utvyakta.com>
2024-09-05 10:41:37 -0400

    eifmksd

--- A1.c
+++ A1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+//using for loops to divide the change into different combinations
 void print_combinations(int cents) {
     for (int quarters = cents / 25; quarters >= 0; quarters--) {
-        int remaining_after_quarters = cents - quarters * 25;
-        for (int dimes = remaining_after_quarters / 10; dimes >= 0; dimes--) {
-            int remaining_after_dimes = remaining_after_quarters - dimes * 10;
-            for (int nickels = remaining_after_dimes / 5; nickels >= 0; nickels--) {
-                int pennies = remaining_after_dimes - nickels * 5;
+        int rem_after_quarters = cents - quarters * 25;
+        for (int dimes = rem_after_quarters / 10; dimes >= 0; dimes--) {
+            int rem_after_dimes = rem_after_quarters - dimes * 10;
+            for (int nickels = rem_after_dimes / 5; nickels >= 0; nickels--) {
+                int pennies = rem_after_dimes - nickels * 5;
                 printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n", quarters, dimes, nickels, pennies);
             }
         }
@@ -15,9 +16,9 @@ void print_combinations(int cents) {
 
 int main() {
     int cents;
-
+//readin the input given to us
     scanf("%d", &cents);
-
+//applying the function on the input
     print_combinations(cents);
 
     return 0;
